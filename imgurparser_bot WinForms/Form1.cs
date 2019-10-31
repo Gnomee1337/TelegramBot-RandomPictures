f18e455de98d9f2d3843f91227b9a6f3bbe86d4d
Gnome <alexeynshah@gmail.com>
2019-10-31 23:16:46 +0200

    Bot

---
+++ imgurparser_bot WinForms/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imgurparser_bot_WinForms
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bw;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.bw = new BackgroundWorker(); //Иниц переменной в конструкторе
+            this.bw.DoWork += this.bw_DoWork; // метод bw_DoWork будет работать асинх
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var text = txtKey.Text; // получаем содержимое текстового поля txtKey в переменную text
+            if (text != "" && this.bw.IsBusy != true) //если не запущен
+            {
+                this.bw.RunWorkerAsync(text);//запускаем, передаем эту переменную в виде аргумента методу bw_DoWork
+            }
+        }
+        private void button1_DoWork(object sender, EventArgs e)
+        {
+            var worker = sender as BackgroundWorker; // получаем ссылку на класс вызываюший событие
+        }
+        async void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            var key = e.Argument as String; // получаем ключ аргументов
+            try
+            {
+                var Bot = new Telegram.Bot.TelegramBotClient(key); // инициализируем API
+                await Bot.SetWebhookAsync(""); //Bot.SetWebhook(""); // Обязательно! убираем старую привязку к вебхуку для бота
+                int offset = 0; // отступ по сообщениям
+                while (true)
+                {
+                    var newUpdates = await Bot.GetUpdatesAsync(offset); // получаем массив обновлений
+                    var updates = newUpdates.Distinct().ToArray(); // убираем повторения из списка новых запросов
+                    foreach (var update in updates) // Перебираем все обновления
+                    {
+                        var message = update.Message;
+                        if (message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                        {
+                            if (message.Text == "/getimage" || message.Text == "/getimage@rndpicture_bot")
+                            {
+                                ImgurPars imgurPars = new ImgurPars();
+                                // в ответ на команду /getimage выводим картинку
+                                await Bot.SendPhotoAsync(message.Chat.Id, imgurPars.GeneratePictur());
+                            }
+                        }
+                        offset = update.Id + 1;
+                    }
+                }
+            }
+            catch (Telegram.Bot.Exceptions.ApiRequestException ex)
+            {
+                Console.WriteLine(ex.Message); // если ключ не подошел - пишем об этом в консоль отладки
+            }
+        }
+    }
+}
---
+++ imgurparser_bot WinForms/ImgurPars.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Web;
+
+namespace imgurparser_bot_WinForms
+{
+    class ImgurPars
+    {
+        Random rnd = new Random();
+
+        string url;
+        string filename;
+        string highLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        //string lowerLetters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        string randomNewDirectory = "Photos" + DateTime.Now.ToString("dd.MM.yyyyInhh-mm-ss");
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        int position = 0;
+        int length;
+
+        WebClient client = new WebClient();
+        StringBuilder sb;
+
+        int[] invalidSizeImages = new int[] { 0, 503, 5082, 4939, 4940, 4941, 12003, 5556 };
+
+        public string GeneratePictur()
+        {
+            Directory.CreateDirectory(@currentDirectory + @randomNewDirectory);
+            bool validation = true;
+            try
+            {
+                while (validation)
+                {
+                    //url = "http://img.prntscr.com/img?url=http://i.imgur.com/";
+                    url = "https://i.imgur.com/";
+
+                    length = rnd.Next(5, 6);
+                    sb = new StringBuilder(length - 1);
+
+                    if (length == 5) // 5
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            //получаем случайное число от 0 до последнего символа в строке length
+                            position = rnd.Next(0, highLetters.Length - 1);
+                            //добавляем выбранный символ в объект StringBuilder
+                            sb.Append(highLetters[position]);
+                        }
+                        url += sb;
+                    }
+                    else // 3
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            //получаем случайное число от 0 до последнего символа в строке length
+                            position = rnd.Next(0, highLetters.Length - 1);
+                            //добавляем выбранный символ в объект StringBuilder
+                            sb.Append(highLetters[position]);
+                        }
+                        url += sb;
+                    }
+                    url += ".jpg";
+                    //Console.WriteLine("Url:" + url);
+
+                    //Console.WriteLine("CurrentDirectory:" + currentDirectory);
+
+                    filename = url.Split('/').Last();
+                    //Console.WriteLine("FileName:" + filename);
+
+                    client.DownloadFile(new Uri(url), @currentDirectory + @randomNewDirectory + "\\" + filename);
+                    //client.DownloadFile(new Uri(url), "\\" + filename);
+                    //Console.WriteLine(@currentDirectory + @randomNewDirectory + "\\" + filename);
+
+                    FileInfo f = new FileInfo(@currentDirectory + @randomNewDirectory + "\\" + filename);
+                    long file_size = f.Length;
+                    //Console.WriteLine("Size:" + file_size);
+                    for (int i = 0; i < invalidSizeImages.Length; i++)
+                    {
+                        if (invalidSizeImages[i] == file_size)
+                        {
+                            Console.WriteLine("[-] Invalid " + url);
+                            File.Delete(@currentDirectory + @randomNewDirectory + "\\" + filename);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("[+] Valid " + url);
+                            if (i == (invalidSizeImages.Length) - 1)
+                            {
+                                validation = false;
+                            }
+                        }
+                    }
+                    //Console.WriteLine("After Check Size:" + file_size);
+                }
+                File.Delete(@currentDirectory + @randomNewDirectory + "\\" + filename);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " ROFLAN " + url);
+            }
+            return url;
+        }
+    }
+}
